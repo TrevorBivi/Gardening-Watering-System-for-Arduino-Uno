85a393b7aa0fa55a9c862f29410818f2f692f258
Trevor Bivi <trevorbivi@cmail.carleton.ca>
2018-01-10 22:09:50 -0500

    Update Garden_Watering_System.ino

--- Garden_Watering_System.ino
+++ Garden_Watering_System.ino
@@ -2,6 +2,7 @@
 TO DO:
 -The system currently runs much faster than it will in the final version. This needs to be changed
 -Make a javascript web browser extention to better format the site
+-Remove Serial
 */
 
 
